bf71f95df6844502872ac055dae059180be592be
Thiwanon Chomcharoen <gmgame.1525@gmail.com>
2020-10-14 20:23:43 +0700

    :fire: remove init function

--- xlog.go
+++ xlog.go
@@ -11,10 +11,6 @@ import (
 
 var logFormat *logrus.TextFormatter
 
-func init() {
-	DefaultLogFormatter()
-}
-
 // DefaultLogFormatter set Formatter with TextFormatter that log like.
 // You can use logrus package to set it with yourself
 func DefaultLogFormatter() {
